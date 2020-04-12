904129c55aea196c1e90af18b9bd42495b68333e
Andrzej Smolinski <anterroz@gmail.com>
2020-04-12 19:12:29 +0200

    First plots

--- disk_analyzer.py
+++ disk_analyzer.py
@@ -1,42 +1,38 @@
 import os
+import matplotlib.pyplot as plt
 
 directory_sizes_dict = {}
 
-def get_size(start_path = ".",size_previous = 2000):
+def get_size(start_path = ".", did_add=True):
     paths_names = os.listdir(start_path)
-    print(paths_names)
-    did_add = "no"
-    total_sub_dir_size = 0
     for root, dirnames, filenames in os.walk(start_path):
-        for dirnames in paths_names:
-            size_kb = 0
-            size_mb = 0
-            path = os.path.join(root,dirnames)
-            print(path)
-            for sub_root, d, f in os.walk(path):
-                for current_file in f:
-                    ffile = os.path.join(sub_root,current_file)
-                    size_kb += os.path.getsize(ffile)
-                size_mb = round( size_kb / 10**6 )
-
-                    #TODO Delete paths name cause scaned
-
-                    
-                if size_mb >= 10000:
-                    get_size(path,size_mb)
-                elif size_mb >= 3000:
-                    print(f"Adding {dirnames} folder to dictonary")
-                    directory_sizes_dict.update({path: size_mb})
-                    did_add = "yes"
-        break
-                # elif (size_previous - total_sub_dir_size) >= 10000 and not paths_names:
-                #     print("listEmptyAndBiggerSize")
-
+        size_kb = 0
+        sub_size = 0
+        if not dirnames:
+            sub_size = 0
+        else:  
+            for d in dirnames:
+                dp = os.path.join(root,d)
+                sub_size += get_size(dp)
+        for f in filenames:
+            fp = os.path.join(root,f)
+            if not os.path.islink(fp):
+                size_kb += os.path.getsize(fp)
+        size_mb = round( size_kb / 10**6 )
+        print(f"Total size for {root} is {size_mb}mb in files and {sub_size}mb in subfolders")
+        if did_add:
+            return size_mb + sub_size
+        elif sub_size < 10000 and size_mb + sub_size > 10000:
+            # print(f"The size for {root} is {size_mb+sub_size}")      
+            directory_sizes_dict.update({root: size_mb+sub_size})
                 
 
 
-                
 if __name__ == '__main__':  
-    get_size('D:\GOPRO\GoPro\Białka')
-    print(directory_sizes_dict)
+    get_size('D:\GOPRO',False)
 
+    directory_sizes_dict = dict(sorted(directory_sizes_dict.items(), key= lambda x: [x[1], x[0]], reverse=False))
+    plt.barh(range(len(directory_sizes_dict)), list(directory_sizes_dict.values()), align='center')
+    plt.title('Folders taking the most space')
+    plt.xlabel('Size')
+    plt.show()
--- testing.py
+++ testing.py
@@ -1,40 +1,15 @@
 import os
+import matplotlib.pyplot as plt
 
-directory_sizes_dict = {}
-
-def get_size(start_path = ".", did_add=True):
-    paths_names = os.listdir(start_path)
-    for root, dirnames, filenames in os.walk(start_path):
-        size_kb = 0
-        sub_size = 0
-        print(f"FOR {root}")
-        if not dirnames:
-            sub_size = 0
-        else:  
-            for d in dirnames:
-                dp = os.path.join(root,d)
-                print(dp)
-                sub_size += get_size(dp)
-                print(f"{dp} is adding {sub_size}")
-        for f in filenames:
-            fp = os.path.join(root,f)
-            if not os.path.islink(fp):
-                size_kb += os.path.getsize(fp)
-        size_mb = round( size_kb / 10**6 )
-        print(sub_size)
-        print(size_mb)
-        if sub_size == 0:
-            if(did_add):
-                return size_mb
-            elif size_mb > 10000:
-                print(f"The size for {root} is {size_mb+sub_size}")
-                directory_sizes_dict.update({root: size_mb+sub_size})
-        elif sub_size < 10000:
-            print(f"The size for {root} is {size_mb+sub_size}")
-            directory_sizes_dict.update({root: size_mb+sub_size})
+dics = {
+    'D:\\GOPRO\\GoPro\\Białka\\Po': 13534,
+    'D:\\GOPRO\\GoPro\\Białka\\Przed\\Nowy folder': 15170,
+    'D:\\GOPRO\\XIAOMI\\GOPRO': 11306
+ }
                 
 
 
 if __name__ == '__main__':  
-    get_size('D:\GOPRO\GoPro\Białka',False)
-    print(directory_sizes_dict)
+    dics = dict(sorted(dics.items(), key= lambda x: [x[1], x[0]], reverse=True))
+    print(dics)
+    
