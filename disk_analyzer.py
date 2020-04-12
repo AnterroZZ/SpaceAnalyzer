a89385ec907ac34f4940a89d6d50c4cf962eee25
Andrzej Smolinski <anterroz@gmail.com>
2020-04-12 10:19:52 +0200

    Again

--- disk_analyzer.py
+++ disk_analyzer.py
@@ -1,36 +1,42 @@
 import os
-def get_precise_size(starting_path):
-    total_size = 0
-    for root, dirnames, filenames in os.walk(starting_path): 
-            for f in filenames:
-                ffile = os.path.join(root,f)
-                total_size += os.path.getsize(ffile)
-    return total_size
+import os
 
-def get_size(start_path = "."):
+direcotry_sizes_dict = {}
 
-    directory_sizes_dict = {}
-    # for root, dirnames, filenames in os.walk(start_path):
-    for firstFolders in os.listdir(start_path):
-        print(f"Calculating size for \'{firstFolders}\' directory'")
-        # for root, dirnames, filenames in os.walk(os.path.join(start_path,firstFolders)): 
-        #     for f in filenames:
-        #         ffile = os.path.join(root,f)
-        #         total_size += os.path.getsize(ffile)
-        path = os.path.join(start_path,firstFolders)
-        size_of_folder = get_precise_size(path)
-        size_mb = round( size_of_folder / 10**6)
-        if(size_mb >= 2000):
-            for root, dirnames, filenames in os.walk(path):
-                for dirs in dirnames:
-                    print(os.path.join(root,dirs))
+def get_size(start_path = ".",size_previous = 2000):
+    did_add = "no"
+    total_sub_dir_size = 0
+    for root, dirnames, filenames in os.walk(start_path):
+        paths_names = os.listdir(start_path)
+        for dirs in dirnames:
+            size_kb = 0
+            size_mb = 0
+            if dirs in paths_names:
+                path = os.path.join(root,dirs)
+                print(path)
+                for sub_root, d, f in os.walk(path):
+                    for current_file in f:
+                        ffile = os.path.join(sub_root,current_file)
+                        size_kb += os.path.getsize(ffile)
+                    size_mb = round( size_kb / 10**6 )
+
+                    #TODO Delete paths name cause scaned
+
                     
+                    if size_mb >= 10000:
+                        get_size(path,size_mb)
+                    elif size_mb >= 3000:
+                        print(f"Adding {dirs} folder to dictonary")
+                        directory_sizes_dict.update({path: size_mb})
+                        did_add = "yes"
+                
+                elif (size_previous - total_sub_dir_size) >= 10000 and not paths_names:
+                    print("listEmptyAndBiggerSize")
 
-        directory_sizes_dict.update({path: f'{size_mb} mb'})    
-    # print(directory_sizes_dict)
-            
+                
 
-            
+
+                
 
 if __name__ == '__main__':  
     get_size('D:\GOPRO\GoPro\Białka')
