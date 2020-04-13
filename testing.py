a15587e6f7f73ccede8d44bf64e2bdda1e3d1cc7
Andrzej Smolinski <anterroz@gmail.com>
2020-04-13 16:46:27 +0200

    Merging script files to one

--- testing.py
+++ testing.py
@@ -2,14 +2,19 @@ import os
 import matplotlib.pyplot as plt
 
 dics = {
-    'D:\\GOPRO\\GoPro\\Białka\\Po': 13534,
-    'D:\\GOPRO\\GoPro\\Białka\\Przed\\Nowy folder': 15170,
-    'D:\\GOPRO\\XIAOMI\\GOPRO': 11306
+    'D:\\GOPRO\\GoPro\\Białka\\Pierwszy': 15170,
  }
+sub_dics = ['D:\\GOPRO\\GoPro\\Białka']
                 
 
 
 if __name__ == '__main__':  
-    dics = dict(sorted(dics.items(), key= lambda x: [x[1], x[0]], reverse=True))
-    print(dics)
+    for root, dirnames, filenames in os.walk('D:\GOPRO\GoPro\Białka'):
+        print(root)
+        if root in sub_dics:
+            for d in dirnames:
+                pt = os.path.join(root,d)
+                print(pt)
+                sub_dics.append(pt)
+    print(sub_dics)
     
